3a7a13b4e78ccd966aacb09131a362534d64d7d4
Sabinodm <sabinodemaio@gmail.com>
2025-04-14 11:42:51 +0200

    INFO: Added dropdown component

---
+++ src/components/DropDownButton/DropDownButton.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import './DropDownButton.css';
+
+interface DropDownButtonProps {}
+
+const DropDownButton: FC<DropDownButtonProps> = () => (
+  <div className="DropDownButton">
+    DropDownButton Component
+  </div>
+);
+
+export default DropDownButton;
