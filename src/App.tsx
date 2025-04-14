2ecd74f9101d28a722f0f2758d616e7cc1b5dc70
Sabinodm <sabinodemaio@gmail.com>
2025-04-14 11:55:07 +0200

    FEAT: Added MainBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import PromoTopBar from "./components/PromoTopBar/PromoTopBar";
+import MainBar from "./components/MainBar/MainBar";
 
 function App() {
-  return (
-    <div className="App">
-      <PromoTopBar />
-    </div>
-  );
+    return (
+        <div className="App">
+            <PromoTopBar/>
+            <MainBar/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/MainBar/MainBar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './MainBar.css';
+import {Container, Image, Navbar} from "react-bootstrap";
+
+export default function MainBar(){
+    return (
+        <Container className="header">
+            <div className="header-content">
+                <Image src="https://picsum.photos/230/97" alt="Logo" className="logo"/>
+                <Image src="https://picsum.photos/728/90" alt="Logo" className="sponsor"/>
+            </div>
+        </Container>
+    );
+}
