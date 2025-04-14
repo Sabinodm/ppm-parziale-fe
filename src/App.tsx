55bcf661df310273b3e0bfe42408e3620a449f16
Sabinodm <sabinodemaio@gmail.com>
2025-04-14 11:37:07 +0200

    FEAT: Added PromoTopBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import PromoTopBar from "./components/PromoTopBar/PromoTopBar";
 
 function App() {
   return (
     <div className="App">
+      <PromoTopBar />
     </div>
   );
 }
---
+++ src/components/PromoTopBar/PromoTopBar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import  './PromoTopBar.css';
+
+
+export default function PromoTopBar() {
+    return (
+        <div className="top-bar">
+            <div className="promo-top-bar">
+                <a className="top-bar-link" href="#">BEST OF 2025 NOMINATION ARE NOW OPEN!</a>
+            </div>
+        </div>
+    );
+}
