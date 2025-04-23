3a016133c8de4a7573b51107c9aa1cbd519cc5d7
Sabinodm <sabinodemaio@gmail.com>
2025-04-23 22:57:21 +0200

    FEAT: Added minimized nav button

--- src/components/MinimizedNavButton/MinimizedNavButton.tsx
+++ src/components/MinimizedNavButton/MinimizedNavButton.tsx
@@ -1,11 +1,17 @@
 import React from "react";
+import './MinimizedNavButton.css';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faChevronLeft} from '@fortawesome/free-solid-svg-icons';
 
 export default function MinimizedNavButton ({label, buttonLink} : {label: string, buttonLink: string}) {
     return (
-        <div className="nav-item">
-            <a href={buttonLink} className="side-nav-button" aria-expanded="false">
-                {label}
-            </a>
+        <div className="minimized-nav-item">
+            <div className="side-nav-wrapper">
+                <a href={buttonLink} className="side-nav-button" aria-expanded="false">
+                    {label}
+                </a>
+                <FontAwesomeIcon icon={faChevronLeft} className="chevron" />
+            </div>
         </div>
 
     );
