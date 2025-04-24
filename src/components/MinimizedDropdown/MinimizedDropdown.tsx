dcd7f91ec156c148be6cbedeb5825714cd0e525d
Sabinodm <sabinodemaio@gmail.com>
2025-04-24 15:15:16 +0200

    FEAT: Modified minimized nav

--- src/components/MinimizedDropdown/MinimizedDropdown.tsx
+++ src/components/MinimizedDropdown/MinimizedDropdown.tsx
@@ -8,13 +8,21 @@ interface DropdownParameters {
     item: DropdownInterface
 }
 export default function MinimizedDropDown ({item} : DropdownParameters) {
+
+    function toggleOpen() {
+        const open = document.getElementById('side-dropdown');
+        if (open) {
+            open.classList.toggle('open-dropdown');
+        }
+    }
+
     return (
         <div className="minimized-item dropdown">
             <>
-                <div className="side-button">
+                <div onClick={toggleOpen} className="side-button">
                     <MinimizedNavButton label={item.key} buttonLink={item.buttonLink}/>
                 </div>
-                <div className="side-nav-wrapper">
+                <div className="side-nav-wrapper" id="side-dropdown">
                     <ul className="side-dropdown-menu">
                         {item.values.map((value: any, index: number) => (
                             <a className="side-dropdown-item" href={value.link}>{value.name}</a>
