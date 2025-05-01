e2d6d02a57c90f897d9866fb506f16aa36c31180
Sabinodm <sabinodemaio@gmail.com>
2025-05-01 12:21:39 +0200

    FIX: modified background scroll

--- src/components/BackgroundPromo/BackgroundPromo.tsx
+++ src/components/BackgroundPromo/BackgroundPromo.tsx
@@ -5,9 +5,7 @@ import {Image} from "react-bootstrap";
 export default function backgroundPromo () {
     return (
         <div className="background-promo" >
-            <a className="link-sponsor" href="#" title='Promotional Content' >
-                <Image src="https://picsum.photos/1950/1000" alt="Promotional Content"/>
-            </a>
+            <a className="background-sponsor" href="#" title='Promotional Content' ></a>
         </div>
     );
 }
---
+++ src/components/Events/Events.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import './Events.css';
+
+interface EventsProps {}
+
+const Events: FC<EventsProps> = () => (
+  <div className="Events">
+    Events Component
+  </div>
+);
+
+export default Events;
