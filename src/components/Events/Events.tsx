1bb3cf88114835f53f4568baa098da7310bcc529
Sabinodm <sabinodemaio@gmail.com>
2025-05-01 16:14:06 +0200

    FEAT: added event section

--- src/components/Events/Events.tsx
+++ src/components/Events/Events.tsx
@@ -1,12 +1,31 @@
-import React, { FC } from 'react';
 import './Events.css';
+import React from "react";
+import {Image} from "react-bootstrap";
 
-interface EventsProps {}
 
-const Events: FC<EventsProps> = () => (
-  <div className="Events">
-    Events Component
-  </div>
-);
+export default function Events () {
+    return (
+        <div className="events-container" >
+            <div className="events-wrapper">
+                <div className="events-header">
+                    <h1 className="event-banner">
+                        <a className="event-banner-link" href="#">Events</a>
+                    </h1>
+                    <a className="event-image-link" href="#" title='photo related to the article'  >
+                        <Image src="https://picsum.photos/654/528"  className="event-image" alt="Event image" />
+                    </a>
+                    <h2 className="event-title">
+                        <a className="event-link" href="#" >Lorem ipsum dolor sit amet, consectetur adipiscing elit</a>
+                    </h2>
+                    <p className="event-description">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    </p>
+                    <div className="event-footer">
+                        <time className="event-date">Sat.,May 3,5-8 p.m.</time>
+                    </div>
 
-export default Events;
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,12 +1,14 @@
 import './HomePage.css';
 import React from "react";
 import TopSection from "../TopSection/TopSection";
+import Events from "../Events/Events";
 
 export default function HomePage () {
     return (
         <div className="home-container" >
             <div className="home-wrapper">
                 <TopSection/>
+                <Events/>
             </div>
         </div>
     );
